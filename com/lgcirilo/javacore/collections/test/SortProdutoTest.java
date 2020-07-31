bcea92473e5ee08e4724df2632c7754316aeaa6f
lgcirilo <lgcirilo@gmail.com>
2020-07-31 19:00:36 -0300

    Collections.sort com Comparator

--- com/lgcirilo/javacore/collections/test/SortProdutoTest.java
+++ com/lgcirilo/javacore/collections/test/SortProdutoTest.java
@@ -5,6 +5,7 @@ import com.lgcirilo.javacore.collections.classes.Produto;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class SortProdutoTest {
     public static void main(String[] args) {
@@ -17,5 +18,17 @@ public class SortProdutoTest {
         for (Produto produto : produtos) {
             System.out.println(produto);
         }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(1);
+        pq.add(3);
+        pq.add(2);
+        pq.add(4);
+        pq.add(2);
+        pq.add(3);
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
+        System.out.println(pq.poll());
+        System.out.println(pq.peek());
     }
 }
